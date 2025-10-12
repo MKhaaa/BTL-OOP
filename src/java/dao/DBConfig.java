8bef3d536eee9f541871e397f6601daefd8d9814
Nguyen Manh Kha <nguyenmanhkha3225@gmail.com>
2025-10-12 23:33:08 +0700

    commit

--- src/java/dao/DBConfig.java
+++ src/java/dao/DBConfig.java
@@ -9,5 +9,8 @@ package dao;
  * @author Admin
  */
 public class DBConfig {
-    
+    public static String driver = "com.mysql.cj.jdbc.Driver";
+    public static String url = "jdbc:mysql://localhost:3306/web_phone";
+    public static String user = "root";
+    public static String password = "322005";
 }
