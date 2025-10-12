efb12964722e3db74b0f08093174c28147db7e13
ManhKha05 <nguyenmanhkha3225@gmail.com>
2025-10-12 23:35:05 +0700

    Update DBConfig.java

--- src/java/dao/DBConfig.java
+++ src/java/dao/DBConfig.java
@@ -10,7 +10,7 @@ package dao;
  */
 public class DBConfig {
     public static String driver = "com.mysql.cj.jdbc.Driver";
-    public static String url = "jdbc:mysql://localhost:3306/web_phone";
+    public static String url = "jdbc:mysql://localhost:3306/webphoneoop";
     public static String user = "root";
     public static String password = "322005";
 }
