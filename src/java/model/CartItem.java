277efb9aace117a542f4d0e06cd64e8ea715cc53
duckissme <mingduck6325@gmail.com>
2025-10-25 11:48:35 +0700

    Cập nhật CartItem.java

--- src/java/model/CartItem.java
+++ src/java/model/CartItem.java
@@ -1,13 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
-
-/**
- *
- * @author Admin
- */
 public class CartItem {
+    private int productId;
+    private String name;
+    private String img;
+    private double price;
+    private int quantity;
+
+    public CartItem(int productId, String name, String img, double price, int quantity) {
+        this.productId = productId;
+        this.name = name;
+        this.img = img;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public double getPrice() {
+        return price;
+    }
     
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+    public void increaseQuantity(){
+        this.quantity += 1;
+    }
+    
+    public void decreaseQuantity(){
+        this.quantity -= 1;
+    }
+    
+    public double getTotalPrice(){
+        return this.price * quantity;
+    }  
 }
